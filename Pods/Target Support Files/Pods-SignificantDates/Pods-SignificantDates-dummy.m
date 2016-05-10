cdf17aad9ae0d8d540b4dffc627ab57c72680eca
Rommel Rico <rommeltj@gmail.com>
2016-05-10 09:30:53 -0700

    Initial Commit

---
+++ Pods/Target Support Files/Pods-SignificantDates/Pods-SignificantDates-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SignificantDates : NSObject
+@end
+@implementation PodsDummy_Pods_SignificantDates
+@end
---
+++ Readme.md
@@ -0,0 +1,4 @@
+# SignificantDates
+
+Tutorial from https://www.raywenderlich.com/15916/how-to-synchronize-core-data-with-a-web-service-part-1 updated to iOS 9.3.
+
